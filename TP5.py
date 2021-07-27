2db2ccd6c1c8eb4fe1bbc630944b18f1bda13ec4
Guts76 <86952489+Guts76@users.noreply.github.com>
2021-07-27 16:02:38 +0200

    Add files via upload

---
+++ TP5.py
@@ -0,0 +1,7 @@
+# TP 5
+
+for i in range (1,11):
+    print("\t Table de",i,":")
+    for j in range (1,10):
+        print("\t\t -",i,"x",j,"=",i*j)
+    print("\n")   # permet de faire l'espace avec la prochaine table de multiplication  
